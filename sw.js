f8c6d73081ddb40c5813a928efb378679dbc4242
Grant Klich <kgrant17@vt.edu>
2021-08-19 22:46:01 -0400

    init

---
+++ sw.js
@@ -0,0 +1,30 @@
+var cacheName = 'numberstack';
+var filesToCache = [
+	'index.html',
+	'img/bow.png',
+	'img/violin2.png',
+	'img/asadsong.mp3',
+
+];
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(filesToCache);
+    }));
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+    caches.match(event.request, {
+        ignoreSearch: true
+    }).then( function(response)  {
+        return response || fetch(event.request);
+    }));
+});
